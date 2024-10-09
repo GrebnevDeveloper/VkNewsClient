f19a8f7d2837fc94b9f6d47d969bd92c6e335522
grebnev <grebnev.developer@gmail.com>
2024-10-09 15:55:10 +0400

    Add PostCard.kt

---
+++ app/src/main/java/com/grebnev/vknewsclient/PostCard.kt
@@ -0,0 +1,135 @@
+package com.grebnev.vknewsclient
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.grebnev.vknewsclient.ui.theme.VkNewsClientTheme
+
+@Composable
+fun PostCard() {
+    Card {
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Lorem ipsum dolor sit amet? consectetur adipiscing elit.")
+            Spacer(modifier = Modifier.height(10.dp))
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(R.drawable.post_content_image),
+                contentDescription = "Post content image",
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Statistics()
+        }
+    }
+}
+
+@Composable
+private fun Statistics() {
+    Row {
+        Row(modifier = Modifier.weight(1f)) {
+            IconWithText(R.drawable.ic_views_count, "690")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(R.drawable.ic_share, "7")
+            IconWithText(R.drawable.ic_comment, "8")
+            IconWithText(R.drawable.ic_like, "23")
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(50.dp),
+            painter = painterResource(R.drawable.post_comunity_thumbnail),
+            contentDescription = "Post community thumbnail"
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "dev/null",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "More vert",
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark() {
+    VkNewsClientTheme(darkTheme = true) {
+        PostCard()
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLight() {
+    VkNewsClientTheme(darkTheme = false) {
+        PostCard()
+    }
+}
