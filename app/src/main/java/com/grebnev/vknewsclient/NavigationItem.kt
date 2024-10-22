7258104ad3b8bafe2cd61bfdfe98e8d0b6a8c6da
grebnev <grebnev.developer@gmail.com>
2024-10-22 13:46:13 +0400

    Add navigation composable

--- app/src/main/java/com/grebnev/vknewsclient/NavigationItem.kt
+++ app/src/main/java/com/grebnev/vknewsclient/NavigationItem.kt
@@ -5,20 +5,25 @@ import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.grebnev.vknewsclient.navigation.Screen
 
 sealed class NavigationItem(
+    val screen: Screen,
     val titleResId: Int,
     val icon: ImageVector
 ) {
-    object Home : NavigationItem(
+    data object Home : NavigationItem(
+        screen = Screen.NewsFeed,
         titleResId = R.string.navigation_item_main,
         icon = Icons.Outlined.Home
     )
-    object Favourite : NavigationItem(
+    data object Favourite : NavigationItem(
+        screen = Screen.Favourite,
         titleResId = R.string.navigation_item_favourite,
         icon = Icons.Outlined.Favorite
     )
-    object Profile : NavigationItem(
+    data object Profile : NavigationItem(
+        screen = Screen.Profile,
         titleResId = R.string.navigation_item_profile,
         icon = Icons.Outlined.Person
     )
