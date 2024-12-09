add3327af89010453a6c42520c5f96adddf40567
grebnev <grebnev.developer@gmail.com>
2024-12-09 13:10:55 +0400

    Add refresh token

--- app/src/main/java/com/grebnev/vknewsclient/presentation/main/AuthState.kt
+++ app/src/main/java/com/grebnev/vknewsclient/presentation/main/AuthState.kt
@@ -1,8 +1,17 @@
 package com.grebnev.vknewsclient.presentation.main
 
-sealed class AuthState {
-    data object Authorized: AuthState()
-    data object NotAuthorized : AuthState()
+import com.vk.id.AccessToken
+import com.vk.id.VKIDAuthFail
 
+sealed class AuthState {
     data object Initial : AuthState()
+
+    data class Authorized(
+        val accessToken: AccessToken
+    ) : AuthState()
+
+    data class NotAuthorized(
+        val fail: VKIDAuthFail
+    ) : AuthState()
+
 }
