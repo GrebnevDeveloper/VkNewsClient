cd69faea6b8684bf49867282948de66175147ade
grebnev <grebnev.developer@gmail.com>
2025-04-12 13:21:59 +0400

    Added icon vk news

--- app/src/main/java/com/grebnev/vknewsclient/presentation/main/auth/VkIdAuthScreen.kt
+++ app/src/main/java/com/grebnev/vknewsclient/presentation/main/auth/VkIdAuthScreen.kt
@@ -45,7 +45,7 @@ fun VkIdAuthScreen(
         ) {
             Image(
                 modifier = Modifier.size(100.dp),
-                painter = painterResource(R.drawable.vk_v2),
+                painter = painterResource(R.drawable.ic_vk_v2),
                 contentDescription = null,
             )
             Spacer(modifier = Modifier.height(100.dp))
